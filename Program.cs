583e9384f3efcb71dd470ad4ac69ca2f4505bb1c
Ross Larson <zo0o0ot@gmail.com>
2021-05-02 22:09:40 +0000

    adds some spectre console example code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Spectre.Console;
 
 namespace prospect_scraper_mddb_2022
 {
@@ -7,6 +8,30 @@ namespace prospect_scraper_mddb_2022
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            AnsiConsole.Status()
+            .Start("Thinking...", ctx => 
+            {
+                // Simulate some work
+                AnsiConsole.MarkupLine("Doing some work...");
+                System.Threading.Thread.Sleep(1000);
+                
+                // Update the status and spinner
+                ctx.Status("Thinking some more");
+                ctx.Spinner(Spinner.Known.Star);
+                ctx.SpinnerStyle(Style.Parse("green"));
+
+                // Simulate some work
+                AnsiConsole.MarkupLine("Doing some more work...");
+                System.Threading.Thread.Sleep(2000);
+            });
+            AnsiConsole.Render(new BarChart()
+            .Width(60)
+            .Label("[green bold underline]Number of fruits[/]")
+            .CenterLabel()
+            .AddItem("Sample Team 1", 12, Color.Yellow)
+            .AddItem("Sample Team 2", 54, Color.Green)
+            .AddItem("Sample Team 3", 33, Color.Red));
+
         }
     }
 }

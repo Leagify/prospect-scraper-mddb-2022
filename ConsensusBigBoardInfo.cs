917fbcf998c9271430e597f06daaf68b9d7cc26f
Christian Oleson <christian.oleson@edgenuity.com>
2021-10-03 20:40:55 -0500

    Maps Extracted

--- ConsensusBigBoardInfo.cs
+++ ConsensusBigBoardInfo.cs
@@ -1,10 +1,5 @@
-using System;
-using CsvHelper;
-using CsvHelper.Configuration;
-
 namespace prospect_scraper_mddb_2022
 {
-
     public class ConsensusBigBoardInfo
     {
         public string ScrapeDate { get; set; }
@@ -13,29 +8,16 @@ namespace prospect_scraper_mddb_2022
         public int TeamBasedMockDraftsUsed { get; set; }
         public int ProspectCount { get; set; }
         public string LastUpdated { get; set; }
-        
 
         public ConsensusBigBoardInfo(string scrapeDate, int bigboardsUsed, int mockDraftsUsed, int teamBasedMockDraftsUsed, int prospectCount, string lastUpdated)
         {
-            this.ScrapeDate = scrapeDate;
-            this.BigBoardsUsed = bigboardsUsed;
-            this.MockDraftsUsed = mockDraftsUsed;
-            this.TeamBasedMockDraftsUsed = teamBasedMockDraftsUsed;
-            this.ProspectCount = prospectCount;
-            this.LastUpdated = lastUpdated;
+            ScrapeDate = scrapeDate;
+            BigBoardsUsed = bigboardsUsed;
+            MockDraftsUsed = mockDraftsUsed;
+            TeamBasedMockDraftsUsed = teamBasedMockDraftsUsed;
+            ProspectCount = prospectCount;
+            LastUpdated = lastUpdated;
         }
 
-    }
-    public sealed class ConsensusBigBoardInfoMap : ClassMap<ConsensusBigBoardInfo>
-    {
-        public ConsensusBigBoardInfoMap()
-        {
-            Map(m => m.ScrapeDate).Index(0).Name("ScrapeDate");
-            Map(m => m.BigBoardsUsed).Index(1).Name("BigBoardsUsed");
-            Map(m => m.MockDraftsUsed).Index(2).Name("MockDraftsUsed");
-            Map(m => m.TeamBasedMockDraftsUsed).Index(3).Name("TeamBasedMockDraftsUsed");
-            Map(m => m.ProspectCount).Index(4).Name("ProspectCount");
-            Map(m => m.LastUpdated).Index(5).Name("LastUpdated");
-        }
     }
 }
--- SchooltRanking.cs
+++ SchooltRanking.cs
@@ -1,10 +1,5 @@
-using System;
-using CsvHelper;
-using CsvHelper.Configuration;
-
 namespace prospect_scraper_mddb_2022
 {
-
     public class SchoolRanking
     {
         public string RankingDateString { get; set; }
@@ -14,28 +9,20 @@ namespace prospect_scraper_mddb_2022
         public string ProjectedPoints { get; set; }
         public string ProspectCount { get; set; }
 
-        public SchoolRanking(string dateString, string school,  
-                               string state, string conference, string projectedPoints, string prospectCount)
+        public SchoolRanking(
+            string dateString, 
+            string school,
+            string state, 
+            string conference, 
+            string projectedPoints, 
+            string prospectCount)
         {
-            this.School = school;
-            this.RankingDateString = dateString;
-            this.State = state;
-            this.Conference = conference;
-            this.ProjectedPoints = projectedPoints;
-            this.ProspectCount = prospectCount;
-        }
-    }
-
-    public sealed class SchoolRankingMap : ClassMap<SchoolRanking>
-    {
-        public SchoolRankingMap()
-        {
-            Map(m => m.School).Index(0).Name("School");
-            Map(m => m.RankingDateString).Index(1).Name("RankingDateString");
-            Map(m => m.State).Index(2).Name("Projection");
-            Map(m => m.Conference).Index(3).Name("Conference");
-            Map(m => m.ProjectedPoints).Index(4).Name("ProjectedPoints");
-            Map(m => m.ProspectCount).Index(5).Name("ProspectCount");
+            School = school;
+            RankingDateString = dateString;
+            State = state;
+            Conference = conference;
+            ProjectedPoints = projectedPoints;
+            ProspectCount = prospectCount;
         }
     }
 }

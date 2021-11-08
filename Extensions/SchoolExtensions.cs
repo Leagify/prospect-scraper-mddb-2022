93801fb1908f8ca03ed233cff8c9520213ac45c2
Christian Oleson <christian.oleson@edgenuity.com>
2021-11-08 16:55:29 -0600

    Move other static methods to extensions

---
+++ Extensions/SchoolExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prospect_scraper_mddb_2022.Extensions
+{
+    public static class SchoolExtensions
+    {
+        public static string ConvertSchool(this string schoolName)
+        {
+            schoolName = schoolName switch
+            {
+                "Mississippi" => "Ole Miss",
+                "Pittsburgh" => "Pitt",
+                _ => schoolName,
+            };
+            return schoolName;
+        }
+    }
+}
---
+++ Extensions/WriterExtensions.cs
@@ -0,0 +1,29 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace prospect_scraper_mddb_2022.Extensions
+{
+    public static class WriterExtensions
+    {
+        public static void WriteToCsvFile<T>(this IEnumerable<T> data, string fileName)
+        {
+            var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = false
+            };
+            using var stream = File.Open(fileName, FileMode.Append);
+            using var writer = new StreamWriter(stream);
+            using var csv = new CsvWriter(writer, csvConfig);
+            csv.WriteRecords(data);
+        }
+
+        public static void EnsureExists(this string directory)
+        {
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+        }
+    }
+}

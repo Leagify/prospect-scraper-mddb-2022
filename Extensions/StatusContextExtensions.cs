7f82ca3fd05a653dae0bb0138694a76e973056b7
Ross Larson <zo0o0ot@gmail.com>
2022-01-03 05:06:01 +0000

    Change item output to orange for visibility.

--- Extensions/StatusContextExtensions.cs
+++ Extensions/StatusContextExtensions.cs
@@ -204,7 +204,7 @@ namespace prospect_scraper_mddb_2022.Extensions
             .AddItem("Team Mock Drafts", teamMockDrafts, Color.Red)
             .AddItem("Schools", schools, Color.Blue)
             .AddItem("States", states, Color.Aqua)
-            .AddItem("State mismatches", emptyStates.Count, Color.BlueViolet)
+            .AddItem("State mismatches", emptyStates.Count, Color.Orange1)
             );
         }
     }
